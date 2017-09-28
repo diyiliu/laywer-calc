7156ebdbb3c758e9c30a392c1e77b064e0baa68d
DIYILIU <572772828@qq.com>
2017-09-28 16:14:41 +0800

    Initial commit

---
+++ src/main/java/com/diyiliu/Main.java
@@ -0,0 +1,13 @@
+package com.diyiliu;
+
+/**
+ * Description: Main
+ * Author: DIYILIU
+ * Update: 2017-09-28 14:51
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
